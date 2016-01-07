9774922f39bd0fb5498910ed3014f9c73226aea9
Cathret <gnewton.geek@gmail.com>
2016-01-07 21:34:19 +0100

    Added all menu PlayerCommands (basics not working)

--- MyWindowsMediaPlayer/ViewModel/ViewModelPlayer.cs
+++ MyWindowsMediaPlayer/ViewModel/ViewModelPlayer.cs
@@ -10,16 +10,28 @@ namespace MyWindowsMediaPlayer.ViewModel
 {
     public class ViewModelPlayer : ViewModelBase
     {
+        #region MenuFile
+        public RelayCommand OpenFile { get; set; }
+        public RelayCommand OpenRecent { get; set; }
+        #endregion
+
+        #region MenuPlaylist
         public RelayCommand CreatePlaylist { get; set; }
         public RelayCommand OpenPlaylist { get; set; }
         public RelayCommand AddToPlaylist { get; set; }
         public RelayCommand DeleteFromPlaylist { get; set; }
         public RelayCommand ModifyPlaylist { get; set; }
         public RelayCommand DeletePlaylist { get; set; }
+        #endregion
+
+        #region Bibliotheque
         public RelayCommand OpenBibliotheque { get; set; }
+        #endregion
 
         public ViewModelPlayer()
         {
+            OpenFile = new RelayCommand(PlayerCommands.OpenFile_Execute, PlayerCommands.OpenFile_CanExecute);
+            OpenRecent = new RelayCommand(PlayerCommands.OpenRecent_Execute, PlayerCommands.OpenRecent_CanExecute);
             CreatePlaylist = new RelayCommand(PlayerCommands.Create_Execute, PlayerCommands.Create_CanExecute);
             OpenPlaylist = new RelayCommand(PlayerCommands.Open_Execute, PlayerCommands.Open_CanExecute);
             AddToPlaylist = new RelayCommand(PlayerCommands.Add_Execute, PlayerCommands.Add_CanExecute);
