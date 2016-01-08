94c68ee673e31f51e777ddc303f7329c371158e2
Cathret <gnewton.geek@gmail.com>
2016-01-08 12:55:41 +0100

    Added last commit (PlayerCommand and everything else)

---
+++ MyWindowsMediaPlayer/Model/PlayerCommands.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+using MyWindowsMediaPlayer.View;
+
+namespace MyWindowsMediaPlayer.Model
+{
+    public class PlayerCommands
+    {
+        #region MenuFile
+
+        #region OpenFile
+        static public bool OpenFile_CanExecute(object parameter)
+        {
+            return (true);
+        }
+
+        static public void OpenFile_Execute(object parameter)
+        {
+#warning "More to create"
+            MediaElement media = parameter as MediaElement;
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            dlg.InitialDirectory = "c:\\";
+            dlg.Filter = "All supported files (*.mp3,*.wav,*.mpeg,*.wmv,*.avi,*.mp4)|*.mp3;*.wav;*.mpeg;*.wmv;*.avi;*.mp4|All files (*.*)|*.*";
+            dlg.FilterIndex = 1;
+            dlg.RestoreDirectory = true;
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                string fileName = dlg.FileName;
+                media.Source = new Uri(fileName, UriKind.Relative);
+                media.Volume = 1;
+                media.Play();
+                MessageBox.Show("Finished playing media || Filename ? " + fileName);
+            }
+        }
+        #endregion
+
+        #region OpenRecent
+        static public bool OpenRecent_CanExecute(object parameter)
+        {
+#warning "if it exists recent files"
+            return (false);
+        }
+
+        static public void OpenRecent_Execute(object parameter)
+        {
+#warning "Modify himself to add direct link to files"
+        }
+        #endregion
+
+        #endregion
+
+        #region MenuPlaylist
+
+        #region CreatePlaylist
+        static public bool Create_CanExecute(object parameter)
+        {
+            return (true);
+        }
+
+        static public void Create_Execute(object parameter)
+        {
+            #warning "Comment"
+            //var newWin = new NewPlaylistWindow();
+            //newWin.Show();
+        }
+        #endregion
+
+        #region OpenPlaylist
+        static public bool Open_CanExecute(object parameter)
+        {
+            return (true);
+        }
+
+        static public void Open_Execute(object parameter)
+        {
+#warning "Doit retourner la valeur de ce qu'on est cense ouvrir"
+            var newWin = new PlaylistWindow();
+            newWin.Show();
+        }
+        #endregion
+
+        #region AddToPlaylist
+        static public bool Add_CanExecute(object parameter)
+        {
+#warning "Si on est actuellement en train de lire quelque chose"
+            if (parameter == null) return false;
+            return (bool)parameter;
+        }
+
+        static public void Add_Execute(object parameter)
+        {
+#warning "Doit retourner la valeur de ce qu'on est cense utiliser pour l'ajout"
+            var newWin = new PlaylistWindow();
+            newWin.Show();
+        }
+        #endregion
+
+        #region DeleteFromPlaylist
+        static public bool DeleteFrom_CanExecute(object parameter)
+        {
+#warning "Si en train de lire filchier d'une playlist"
+            if (parameter == null) return false;
+            return (bool)parameter;
+        }
+
+        static public void DeleteFrom_Execute(object parameter)
+        {
+#warning "Supprimer actuel si actuel"
+        }
+        #endregion
+
+        #region ModifyPlaylist
+        static public bool Modify_CanExecute(object parameter)
+        {
+            return (true);
+        }
+
+        static public void Modify_Execute(object parameter)
+        {
+#warning "Doit retourner la valeur de ce qu'on est cense modifier"
+            var newWin = new PlaylistWindow();
+            newWin.Show();
+        }
+
+        #endregion
+
+        #region DeletePlaylist
+        static public bool Delete_CanExecute(object parameter)
+        {
+            return (true);
+        }
+
+        static public void Delete_Execute(object parameter)
+        {
+#warning "Doit retourner la valeur de ce qu'on est cense supprimer"
+            var newWin = new PlaylistWindow();
+            newWin.Show();
+        }
+        #endregion
+
+        #endregion
+
+        #region OpenBilbliotheque
+
+        static public bool OpenBiblio_CanExecute(object parameter)
+        {
+            return (true);
+        }
+
+        static public void OpenBiblio_Execute(object parameter)
+        {
+#warning "More to create"
+            //var newWin = new BibliothequeWindow();
+            //newWin.Show();
+        }
+
+        #endregion
+    }
+}
--- MyWindowsMediaPlayer/View/PlayerWindow.xaml.cs
+++ MyWindowsMediaPlayer/View/PlayerWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows;
+using Microsoft.Win32;
 using MyWindowsMediaPlayer.ViewModel;
 
 namespace MyWindowsMediaPlayer.View
@@ -13,6 +14,19 @@ namespace MyWindowsMediaPlayer.View
         {
             InitializeComponent();
             this.DataContext = new ViewModelPlayer();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TOTO");
+            MediaElementPlayer.Source = new Uri("C:\\Users\\Guillaume\\Music\\Lolilol.mp3");
+            MediaElementPlayer.Volume = 1;
+            MediaElementPlayer.Play();
+        }
+
+        private void MediaElementPlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
